a1f4edbec02691100e8b505cbbcfd6507afd2574
Ele-Peng <374237925@qq.com>
2020-11-20 17:28:28 +0800

    feat: grey image

--- image-main-color.js
+++ image-main-color.js
@@ -28,7 +28,7 @@ function getImageData(image) {
     canvas.height = image.height;
     const context = canvas.getContext('2d');
     context.drawImage(image, 0, 0);
-    return context.getImageData(0, 0, image.width, image.height).data;
+    return context.getImageData(0, 0, image.width, image.height);
   } catch (err) {
     return [];
   }
